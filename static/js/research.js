627a832c775e40847f5bda5c5a5c724a88fbb733
Ben Davies <bldavies@users.noreply.github.com>
2020-12-04 22:51:59 -0800

    Hide abstract show/hide button if JavaScript disabled

--- static/js/research.js
+++ static/js/research.js
@@ -1,3 +1,8 @@
+function show_abstract_toggle(id) {
+  t = document.getElementById(id.concat('-abstract-toggle'))
+  t.style.display = "inline-block"
+}
+
 function toggle_abstract(id) {
   var a = document.getElementById(id.concat('-abstract'))
   if (a.style.display == "none") {
