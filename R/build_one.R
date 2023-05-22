60c1f0edbbb52e0bb2fa3210f5d9451091bde079
Ben Davies <git@bldavies.fastmail.com>
2023-05-22 08:48:59 -0700

    Document build_one.R

--- R/build_one.R
+++ R/build_one.R
@@ -3,34 +3,55 @@
 # This script defines the build_one function used in R/build.R.
 #
 # Ben Davies
-# September 2022
+# May 2023
 
 local({
-  a <- commandArgs(TRUE)
-  d <- gsub("^(.*)/(.*?)[.][A-Za-z]+$", "\\1", a[1])
+  
+  # Get input arguments
+  a = commandArgs(T)
+  
+  # Extract post directory
+  post_dir = gsub("^(.*)/(.*?)[.][A-Za-z]+$", "\\1", a[1])
+  
+  # Set knitr options
   knitr::opts_chunk$set(
     fig.path = "figures/",
     fig.cap = "",  # Remove default alt text
-    cache.path = sprintf("%s/cache/", d)
+    cache.path = sprintf("%s/cache/", post_dir)
   )
   knitr::opts_knit$set(
-    base.dir = normalizePath(d, mustWork = TRUE),
+    base.dir = normalizePath(post_dir, mustWork = T),
     base.url = ""
   )
+  
+  # Knit to md
   set.seed(0)
-  knitr::knit(a[1], a[2], quiet = TRUE, encoding = "UTF-8", envir = .GlobalEnv)
+  knitr::knit(a[1], a[2], quiet = T, encoding = "UTF-8", envir = .GlobalEnv)
+  
+  # Post-process knitted file
   if (file.exists(a[2])) {
-    x <- xfun::read_utf8(a[2])
-    y <- xfun::read_utf8(a[1])
-    if (sum(grepl("```", y)) > 0 & sum(grepl("linkSource", y)) == 0) {
-      x <- blogdown:::append_yaml(x, list(linkSource = TRUE))
+    
+    # Read Rmd source and knitted md file
+    infile = xfun::read_utf8(a[1])
+    outfile = xfun::read_utf8(a[2])
+    
+    # Add linkSource to YAML header if Rmd source contains code block
+    if (sum(grepl("```", infile)) > 0 & sum(grepl("linkSource", infile)) == 0) {
+      outfile = blogdown:::append_yaml(outfile, list(linkSource = T))
     }
-    x <- xfun::protect_math(x)
-    if (sum(grepl("`(\\$\\$|\\\\\\()", x)) > 0 & sum(grepl("loadMathJax", y)) == 0) {
-      x <- blogdown:::append_yaml(x, list(loadMathJax = TRUE))
+    
+    # Set up MathJax
+    outfile = xfun::protect_math(outfile)
+    if (sum(grepl("`(\\$\\$|\\\\\\()", outfile)) > 0 & sum(grepl("loadMathJax", infile)) == 0) {
+      outfile = blogdown:::append_yaml(outfile, list(loadMathJax = T))
     }
-    x <- gsub("(\\\n){2,}", "\n\n", paste(x, collapse = "\n"))  # Excess \n's 
-    x <- gsub("(\\\n)+$", "\n", x)  # EoF
-    xfun::write_utf8(x, a[2])
+    
+    # Remove excess whitespace
+    outfile = gsub("(\\\n){2,}", "\n\n", paste(outfile, collapse = "\n"))
+    outfile = gsub("(\\\n)+$", "\n", outfile)
+    
+    # Save post-processed file
+    xfun::write_utf8(outfile, a[2])
+    
   }
 })
