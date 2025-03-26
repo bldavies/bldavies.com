4bd3e49455ff2c57ca206156dd092dac75922f4c
Ben Davies <git@bldavies.fastmail.com>
2025-03-26 10:50:01 -0700

    Reformat CV

--- R/cv.R
+++ R/cv.R
@@ -3,35 +3,24 @@
 # This script builds my CV using data from data/cv.yaml and data/research.yaml.
 #
 # Ben Davies
-# January 2025
+# March 2025
 
 
 # Initialization ----
 
-library(bldr)
+# Load packages
 library(dplyr)
 library(lubridate)
-library(purrr)
-library(rmarkdown)
 library(yaml)
 
-md2tex = function(x) {  # Untested against edge cases
-  x = gsub('[*](.*)[*]', '\\\\emph{\\1}', x)
-  x = gsub('"(.*)"', '``\\1\'\'', x)
-  x
-}
-
+# Define function for constructing date ranges
 get_period = function(start_date, end_date) {
   x = year(start_date)
   y = year(end_date)
-  ifelse(is.na(y), paste0(x, '--present'), ifelse(x < y, paste0(x, '--', y - 2000), x))
+  ifelse(is.na(y), paste0(x, '--'), ifelse(x < y, paste0(x, '--', y - 2000), x))
 }
 
-indata = read_yaml('data/cv.yaml')
-
-
-# Header and footer ----
-
+# Define preamble
 preamble = '\\documentclass[11pt,oneside]{memoir}
 
 \\title{Curriculum Vitae}
@@ -42,21 +31,26 @@ preamble = '\\documentclass[11pt,oneside]{memoir}
 \\usepackage[T1]{fontenc}
 \\usepackage[utf8]{inputenc}
 \\usepackage[margin=1in,includefoot]{geometry}
-\\usepackage{nohyperref}
+\\usepackage[colorlinks,urlcolor=blue]{hyperref}
+\\usepackage{titlesec}
 
-\\makeevenfoot{plain}{\\theauthor}{}{Page~\\thepage\\ of~\\thelastpage}
-\\makeoddfoot{plain}{\\theauthor}{}{Page~\\thepage\\ of~\\thelastpage}
+\\makeevenfoot{plain}{\\today}{}{Page~\\thepage\\ of~\\thelastpage}
+\\makeoddfoot{plain}{\\today}{}{Page~\\thepage\\ of~\\thelastpage}
 \\OnehalfSpacing
 \\pagestyle{plain}
 \\raggedright
-\\setaftersecskip{1em}
-\\setbeforesecskip{2em}
 \\setlength{\\parindent}{0pt}
-\\setsecheadstyle{\\large\\bfseries}
 \\setsecnumdepth{part}
+\\titleformat{\\chapter}{\\Large\\bfseries}{}{}{\\MakeUppercase}[\\titlerule]
+\\titleformat{\\section}{\\large\\bfseries}{}{}{}[\\titlerule]
+\\titleformat{\\subsection}{\\bfseries}{}{}{}
+\\titlespacing{\\chapter}{0pt}{-2em}{1em}
+\\titlespacing{\\section}{0pt}{2em}{1em}
+\\titlespacing{\\subsection}{0pt}{1em}{0pt}
 
-\\newcommand{\\entry}[1]{\\par\\hangindent=0.4in #1}'
+\\newcommand{\\entry}[2]{\\par\\parbox[t]{0.9\\linewidth}{\\strut\\raggedright\\hangindent=2em #2\\strut}\\hfill#1}'
 
+# Define header
 header = c(
   preamble,
   '',
@@ -64,137 +58,192 @@ header = c(
   ''
 )
 
+# Define footer
 footer = c(
   '',
   '\\end{document}'
 )
 
 
-# Contact info ----
+# Body ----
 
-contact_info = with(
-  unlist(indata$addresses, recursive = F),
-  c(
-    '\\parbox{\\textwidth}{%',
-    '\t\\parbox[t]{0.6\\textwidth}{%',
-    paste(paste0('\t\t', strsplit(physical, '\n')[[1]]), collapse = '\\par\n\t'),
-    '\t}',
-    '\t\\hfill',
-    '\t\\parbox[t]{0.3\\textwidth}{\\raggedleft%',
-    sprintf('\t\t%s\\par', email),
-    sprintf('\t\t%s', web),
-    '\t}',
-    '}\\par'
+# Import data
+indata = read_yaml('data/cv.yaml')
+
+
+## Education ----
+
+# Extract data
+education_data = indata$education %>%
+  lapply(function(x) {
+    x$degrees %>%
+      bind_rows() %>%
+      mutate(institution = x$institution)
+  }) %>%
+  bind_rows() %>%
+  group_by(institution) %>%
+  mutate(min_start_date = min(start_date),
+         degree_pos = dense_rank(desc(start_date))) %>%
+  ungroup() %>%
+  mutate(institution_pos = dense_rank(desc(min_start_date)))
+
+# Initialize lines
+education_lines = c()
+
+# Create institution headings
+institution_lines = education_data %>%
+  distinct(institution_pos, institution) %>%
+  arrange(institution_pos) %>%
+  mutate(line = sprintf('\\subsection{%s}', institution)) %>%
+  {.$line}
+
+# Iterate over institutions
+for (i in seq_along(institution_lines)) {
+  
+  # Create degree list
+  degree_lines = education_data %>%
+    filter(institution_pos == i) %>%
+    arrange(degree_pos) %>%
+    mutate(line = sprintf('\\entry{%s}{%s}', get_period(start_date, end_date), title)) %>%
+    {paste0('\t', .$line)}
+  
+  # Update lines
+  education_lines = c(
+    education_lines,
+    '',
+    institution_lines[i],
+    '',
+    degree_lines
   )
-)
+  
+}
 
 
-# Education ----
+## Experience ----
 
-education = indata$degrees %>%
+# Extract data
+experience_data = indata$experience %>%
+  lapply(function(x) {
+    x$roles %>%
+      bind_rows() %>%
+      mutate(employer = x$employer)
+  }) %>%
   bind_rows() %>%
-  arrange(desc(start_date)) %>%
-  mutate(period = get_period(start_date, end_date),
-         text = sprintf('\t\\entry{%s, %s, %s.}', degree, institution, period)) %>%
-  {.$text}
+  group_by(employer) %>%
+  mutate(min_start_date = min(start_date),
+         role_pos = dense_rank(desc(start_date))) %>%
+  ungroup() %>%
+  mutate(employer_pos = dense_rank(desc(min_start_date)))
+
+# Initialize lines
+experience_lines = c()
+
+# Create employer headings
+employer_lines = experience_data %>%
+  distinct(employer_pos, employer) %>%
+  arrange(employer_pos) %>%
+  mutate(line = sprintf('\\subsection{%s}', employer)) %>%
+  {.$line}
+
+# Iterate over employers
+for (i in seq_along(employer_lines)) {
+  
+  # Create degree list
+  role_lines = experience_data %>%
+    filter(employer_pos == i) %>%
+    arrange(role_pos) %>%
+    mutate(line = sprintf('\\entry{%s}{%s}', get_period(start_date, end_date), title)) %>%
+    {paste0('\t', .$line)}
+  
+  # Update lines
+  experience_lines = c(
+    experience_lines,
+    '',
+    employer_lines[i],
+    '',
+    role_lines
+  )
+  
+}
 
 
-# Employment ----
+## Awards ----
 
-employment = indata$jobs %>%
+# Create lines
+award_lines = indata$awards %>%
   bind_rows() %>%
-  arrange(desc(start_date)) %>%
-  mutate(period = paste0(year(start_date), '--', ifelse(!is.na(end_date), year(end_date) - 2000, 'present')),
-         text = sprintf('\t\\entry{%s, %s, %s.}', position, employer, period)) %>%
-  {.$text}
+  mutate(line = sprintf('\\entry{%s}{%s}', year, description)) %>%
+  {paste0('\t', .$line)}
 
 
-# Honours ---
+## Research ----
 
-honours = indata$honours %>%
-  bind_rows() %>%
-  mutate(text = sprintf('%s, %s.', description, year)) %>%
-  {sprintf('\t\\entry{%s}', .$text)}
-
-
-# Research ----
-
-research = read_yaml('data/research.yaml') %>%
+# Extract data
+research_data = read_yaml('data/research.yaml') %>%
   bind_rows() %>%
   arrange(desc(date)) %>%
-  mutate(text = paste0('"', title,
-                       ifelse(!is.na(coauthors), sprintf('" (with %s), ', coauthors), '," '),
-                       ifelse(!is.na(outlet), sprintf('%s, ', outlet), ''),
-                       ifelse(!is.na({if ('forthcoming' %in% names(.)) forthcoming else rep(NA, nrow(.))}), 'forthcoming', substr(date, 1, 4)),
-                       '.'))
+  mutate(headline = paste0(ifelse(grepl('http', url), sprintf('\\href{%s}{%s}', url, title), title),
+                           ifelse(!is.na(coauthors), sprintf(' (with %s)', coauthors), '')))
 
-publications = research %>%
+# Create publication lines
+publication_lines = research_data %>%
   filter(type == 'pub') %>%
-  {sprintf('\t\\entry{%s}', md2tex(.$text))}
+  mutate(line = sprintf('\\entry{%s}{%s, \\emph{%s}}', year(date), headline, gsub('[*]', '', outlet))) %>%
+  {paste0('\t', .$line)}
 
-working_papers = research %>%
+# Create working paper lines
+working_paper_lines = research_data %>%
   filter(type %in% c('wp', 'old')) %>%
-  {sprintf('\t\\entry{%s}', md2tex(.$text))}
+  mutate(line = sprintf('\\entry{%s}{%s}', year(date), headline)) %>%
+  {paste0('\t', .$line)}
 
-notes = research %>%
+# Create note lines
+note_lines = research_data %>%
   filter(type == 'note') %>%
-  {sprintf('\t\\entry{%s}', md2tex(.$text))}
+  mutate(line = sprintf('\\entry{%s}{%s}', year(date), headline)) %>%
+  {paste0('\t', .$line)}
 
 
-# Presentations ----
+## Conference presentations ----
 
-presentations = indata$talks %>%
+# Create lines
+conference_lines = indata$conferences %>%
   bind_rows() %>%
-  arrange(date) %>%
-  group_by(year = year(date)) %>%
-  distinct(location) %>%
-  summarise(text = paste0(paste(location, collapse = '; '), '.')) %>%
-  ungroup() %>%
-  arrange(-year) %>%
-  {sprintf('\t\\entry{\\parbox[b]{0.4in}{\\emph{%d}: }%s}', .$year, .$text)}
+  arrange(desc(date)) %>%
+  mutate(text = ifelse(!is.na(location), sprintf('%s, %s', conf_title, location), conf_title),
+         line = sprintf('\\entry{%s}{%s}', year(date), text)) %>%
+  {paste0('\t', .$line)}
 
 
-# Teaching ----
+## Collation ----
 
-teaching = indata$teaching %>%
-  bind_rows() %>%
-  arrange(desc(start_date)) %>%
-  mutate(period = get_period(start_date, end_date),
-         text = sprintf('\t\\entry{%s, %s, %s.}', position, institution, period)) %>%
-  {.$text}
-
-
-# Combine and compile ----
-
-content = c(
-  '\\begin{center}',
-  '\t{\\Large\\bfseries\\MakeUppercase{\\theauthor}\\par}\\vskip\\beforesecskip',
-  '\\end{center}',
+body = c(
+  '\\chapter{\\theauthor}',
   '',
-  contact_info,
+  sprintf('\tEmail: \\href{mailto:%s}{%s}', indata$email, indata$email),
+  '',
+  sprintf('\tWebsite: \\href{https://%s}{%s}', indata$website, indata$website),
   '',
   '\\section{Education}',
+  education_lines,
   '',
-  education,
+  '\\section{Experience}',
+  experience_lines,
   '',
-  '\\section{Employment}',
+  '\\section{Awards}',
   '',
-  employment,
-  '',
-  '\\section{Honours}',
-  '',
-  honours,
+  award_lines,
   '',
   '\\section{Publications}',
   '',
-  publications,
+  publication_lines,
   '',
   {
-    if (length(working_papers) > 0) {
+    if (length(working_paper_lines) > 0) {
       c(
         '\\section{Working Papers}',
         '',
-        working_papers,
+        working_paper_lines,
         ''
       )
     } else {
@@ -203,23 +252,18 @@ content = c(
   },
   '\\section{Policy and Technical Notes}',
   '',
-  notes,
+  note_lines,
   '',
-  '\\section{Seminars and Conference Presentations}',
+  '\\section{Conference Presentations}',
   '',
-  presentations,
-  '',
-  '\\section{Teaching}',
-  '',
-  teaching,
-  '\\vskip\\beforesecskip',
-  '\\begin{center}',
-  '\t\\footnotesize Last updated: \\today',
-  '\\end{center}'
+  conference_lines
 ) %>%
   {paste0('\t', .)}
 
-writeLines(c(header, content, footer), 'static/cv.tex')
+
+# Compilation ----
+
+writeLines(c(header, body, footer), 'static/cv.tex')
 system('pdflatex -output-directory static static/cv.tex')
 system('pdflatex -output-directory static static/cv.tex')  # Compile twice to define \thelastpage
-system('rm static/cv.aux static/cv.log')
+system('rm static/cv.aux static/cv.log static/cv.out')
